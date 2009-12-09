ebe9892801ac2143c86b99a269aeb2e86337932c
Denis Sokolov <denis_git@sokolov.cc>
2009-12-09 03:01:16 +0200

    Main engine done.

--- htmlify.py
+++ htmlify.py
@@ -25,10 +25,16 @@
 '''
 
 import os
+import re
+import base64
+import mimetypes
+from HTMLParser import HTMLParser
 
 class Error(Exception): pass
 class OverwriteError(Error): pass
 class WritingError(Error): pass
+class MimeError(Error): pass
+class EncodingError(Error): pass
 
 class Htmlifier:
 	force = False
@@ -56,15 +62,92 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 		html = inf.read()
 		inf.close()
 
+		parser = self.__Parser(os.path.dirname(input))
+		parser.feed(html)
+		html = parser.get()
+
 		if self.addGpl3 == True:
 			html = self.__gpl3 + html
-
 		try:
 			outf = open(output, 'w')
 		except IOError:
 			raise WritingError('Destination cannot be opened for writing. Check your permissions.')
 		outf.write(html)
 		outf.close()
+
+	class __Parser(HTMLParser):
+		def __init__(self, path):
+			HTMLParser.__init__(self)
+			self.path = path
+			self.__data = []
+			self.__uriRe = re.compile('^(((http|ftp)s?|file)://|(mailto|gopher):)', re.I)
+
+		def get(self):
+			return ''.join(self.__data)
+
+		def reset(self, *args, **kwds):
+			self.__data = []
+			return HTMLParser.reset(self, *args, **kwds)
+
+		def handle_starttag(self, tag, attrs, selfClose = False):
+			attrs = dict(attrs)
+			if tag == 'script' and 'src' in attrs:
+				js = self.__file(attrs['src'])
+				del attrs['src']
+				self.__data.append('<script%s>%s</script>' % (self.__attrs(attrs), js))
+			elif tag == 'link' and 'rel' in attrs and attrs['rel'] == 'stylesheet' and 'href' in attrs:
+				css = self.__file(attrs['href'])
+				self.__data.append('<style type="text/css">%s</style>' % css)
+			else:
+				if 'src' in attrs:
+					attrs['src'] = self.__uriToData(attrs['src'])
+				if tag == 'link' and 'href' in attrs:
+					attrs['href'] = self.__uriToData(attrs['href'])
+
+				selfClose = selfClose and ' /' or ''
+				self.__data.append('<%s%s%s>' % (tag, self.__attrs(attrs), selfClose))
+
+		def __uriToData(self, uri):
+			if self.__uriRe.match(uri):
+				return uri
+			path = os.path.join(self.path, uri)
+			if not os.path.exists(path) or not os.path.isfile(path):
+				return uri
+			f = open(path)
+			data = f.read()
+			f.close()
+			data = base64.b64encode(data)
+			mime, encoding = mimetypes.guess_type(path, False)
+			if not mime:
+				raise MimeError('Cannot guess MIME type of file "%s". Give it a better name.' % path)
+			if encoding:
+				raise EncodingError('File "%s" appears to be encoded. I can\'t work with encoded files. Yet.' % path)
+			return 'data:%s;base64,%s' % (mime, data)
+
+		def handle_startendtag(self, tag, attrs):
+			self.handle_starttag(tag, attrs, selfClose=True)
+		def handle_endtag(self, tag):
+			self.__data.append('</%s>' % tag)
+		def handle_data(self, data):
+			self.__data.append(data)
+		def handle_charref(self, name):
+			self.__data.append('&#%s;' % name)
+		def handle_entityref(self, name):
+			self.__data.append('&%s;' % name)
+		def handle_decl(self, decl):
+			self.__data.append('<!%s>' % decl)
+		def handle_pi(self, pi):
+			self.__data.append('<?%s>' % pi)
+
+		def __file(self, path):
+			path = os.path.join(self.path, path)
+			f = open(path)
+			data = f.read()
+			f.close()
+			return data
+
+		def __attrs(self, attrs):
+			return ''.join([' %s="%s"' % (k,v) for k,v in attrs.items()])
 
 def main():
 	from optparse import OptionParser
